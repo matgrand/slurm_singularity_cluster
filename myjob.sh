1d3403a7d51c759529a8b1fadc97820df89eadf8
matgrand <matteo.grandin@gmail.com>
2024-05-08 17:08:09 +0200

    +

--- myjob.sh
+++ myjob.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 #SBATCH --job-name=my_first_job
 #SBATCH --error=output.%j.err
-#SBATCH --output=error.%j.out
+#SBATCH --output=output.%j.out
 #SBATCH --partition=allgroups
 #SBATCH --ntasks=1
 #SBATCH --mem=1G
 #SBATCH --time=00:05:00
 #SBATCH --gres=gpu:a40:1
 
+echo "Starting job"
+echo "Working directory is $WORKING_DIR"
+echo "Files: $(ls $WORKING_DIR)"
 cd $WORKING_DIR
 
 srun singularity exec --nv mycontainer.sif python test_script.py
