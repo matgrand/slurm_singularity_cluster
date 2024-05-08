31f6ae069a1bcd3ff58fa687a4242e865f6c8899
matgrand <matteo.grandin@gmail.com>
2024-05-08 11:41:43 +0200

    +

--- create_container.sh
+++ create_container.sh
@@ -2,12 +2,12 @@
 
 # run this script to create a Singularity container on Ubuntu
 
-rm -rf /tmp/lima/test_container.sif
+rm -rf test_container.sif
 
 # build the container (sandbox -> to make modifications to the container later)
 # apptainer build --sandbox test_container.sif pt.def 
 # apptainer build --sandbox test_container.sif numpy.def 
-apptainer build /tmp/lima/test_container.sif amd64_ubu.def 
+apptainer build test_container.sif amd64_ubu.def 
 # apptainer build --sandbox test_container.sif ubu20.def 
 # apptainer build --sandbox test_container.sif ubu22.def 
 
@@ -15,4 +15,4 @@ echo "Container built"
 
 # let's enter the container (writable -> to make mods, cleanenv -> to avoid conflicts with the host
 # environment, fakeroot -> to have root access inside the container)
-apptainer shell --writable --cleanenv --fakeroot /tmp/lima/test_container.sif
+apptainer shell --writable --cleanenv --fakeroot test_container.sif
